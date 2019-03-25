887bfdb973c3ffa8769c391fce80029d1ecfaa62
AlbertoCastellon <acastemi@everis.com>
2019-03-25 12:30:29 +0100

    Basic CRUD of a test Entity (User)

---
+++ src/apiRESTTest/UserList.java
@@ -0,0 +1,13 @@
+package apiRESTTest;
+
+import java.util.List;
+
+public class UserList {
+    private List<User> employees;
+ 
+    public UserList() {
+        employees = new ArrayList<>();
+    }
+ 
+    // standard constructor and getter/setter
+}
